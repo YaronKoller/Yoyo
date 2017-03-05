57a03f45f00e254b5a994922005c08d1891239f9
YaronKoller <yaronkoller@live.ca>
2017-03-05 17:04:51 -0500

    Initial Commit

---
+++ Yoyo/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Yoyo
+//
+//  Created by Yaron Koller on 2017-03-05.
+//  Copyright © 2017 Yaron Koller. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
